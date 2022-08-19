3c910b27bac167761c353f190f0f4885255a0b4c
Сергей <s.v.yakovlev@yandex.ru>
2022-08-19 21:24:12 +0400

    Initial commit

---
+++ src/main/java/yakovlev/MapProject/MapProjectApplication.java
@@ -0,0 +1,13 @@
+package yakovlev.MapProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapProjectApplication.class, args);
+	}
+
+}
